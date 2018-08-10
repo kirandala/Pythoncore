ef4806715187baf798d1f56db61e65d13845dac0
Kiran <kirandala@gmail.com>
2018-08-10 14:09:25 +0530

    functions

--- venv/Include/list.py
+++ venv/Include/list.py
@@ -4,3 +4,22 @@
 lst=[10,20,'gopal','m',40]
 # empty list can be created by []
 print(lst)
+
+'''program to create a list using range() function'''
+
+lst=range(10)
+for i in lst:
+    print(i,end=' ')
+print()# thorw cursor to next line
+
+#create a list from integer from 5 to 8
+lst1=range(5,10)
+for i in lst1:
+    print(i,end=' ')
+print()
+
+#create a list with odd numbers from 5 to 9
+lst2=range(5,9,2) # giving step number 2
+for x in lst2:
+    print(x,end=' ')
+
